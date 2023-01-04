91ed33f90102db64aaf80c3df484d7e61e87a119
mol1986 <mol1986@naver.com>
2023-01-04 15:49:52 +0900

    Navigation

--- Assets/Scripts/InputPanel/InputPanel.cs
+++ Assets/Scripts/InputPanel/InputPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InputPanel : MonoBehaviour
@@ -9,9 +10,28 @@ public class InputPanel : MonoBehaviour
 
     [SerializeField] ContentSizeFitter sizeFitter;
 
+    EventSystem eventSystem;
+
     private void Awake()
     {
-        Instance = this; 
+        Instance = this;
+        eventSystem = EventSystem.current;      // 현재 활성화 되어있는 이벤트 시스템 받기.
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            // 현재 선택 중인 오브젝트.
+            GameObject current = eventSystem.currentSelectedGameObject;     
+            if(current != null)
+            {
+                // 해당 오브젝트에게서 선택자를 검색후 하단 오브젝트를 찾는다.
+                Selectable next = current.GetComponent<Selectable>()?.FindSelectableOnDown();
+                if (next != null)
+                    next.Select();
+            }
+        }
     }
 
     // 컨텐츠의 높이가 변경되는 이벤트 함수.
